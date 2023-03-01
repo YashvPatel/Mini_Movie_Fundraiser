20b9d4f18cdc386ce785b3bc451bb880ff05be99
YashvPatel <89170208+YashvPatel@users.noreply.github.com>
2023-03-02 12:58:46 +1300

    Add files via upload

--- 00_MM_base_v01.py
+++ 00_MM_base_v01.py
@@ -39,17 +39,49 @@ def not_blank(question):
 
 # Calculate the ticket price based on the age
 def calc_ticket_price(var_age):
-    pass
+    # ticket is $7:50 for users under 16
+    if var_age < 16:
+        price = 7.5
+
+    # ticket is $10.50 for users between 16 and 64
+    elif var_age < 65:
+        price = 10.5
+
+    # ticket price is $6:50 for seniors (65+)
+    else:
+        price = 6.5
+
+    return price
 
 
-# main routine starts here
+# check that users enter a valid response (eg: yes / no)
+# cash / credit based on list of options
+def string_checker(question, num_letters, valid_responses):
+    if num_letters == 1:
+        short_version = 1
+    else:
+        short_version = 2
+
+    while True:
+        response = input(question).lower()
+
+        for item in valid_responses:
+            if response == item[:short_version] or response == item:
+                return item
+
+        print(f"Please enter {valid_responses[0]} or {valid_responses[1]}")
+
 
 # set maximum number of tickets below
-MAX_TICKETS = 5
+MAX_TICKETS = 3
 tickets_sold = 0
 
+yes_no_list = ["yes", "no"]
+payment_list = ["cash", "credit"]
+
 # Ask user if they want to see the instructions
-want_instructions = yes_no("Do you want to see the instructions? ")
+want_instructions = string_checker("Do you want to read the instructions (y/n) ",
+                                   1, yes_no_list)
 
 if want_instructions == "yes":
     print("Instructions go here")
@@ -73,6 +105,13 @@ while tickets_sold < MAX_TICKETS:
     else:
         print("?? That looks like a typo please try again.")
         continue
+
+    # calculate ticket cost
+    tickets_cost = calc_ticket_price(age)
+
+    # get payment method
+    pay_method = string_checker("Choose a payment method (cash/ credit): ",
+                                2, payment_list)
 
     tickets_sold += 1
 
